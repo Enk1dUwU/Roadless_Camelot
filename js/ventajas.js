6eb1ce05a0cd450d4c17150eadf42c2a0cbd789c
Enk1dUwU <122658193+Enk1dUwU@users.noreply.github.com>
2023-02-13 04:11:57 +0100

    Roadless Camelot V3

---
+++ js/ventajas.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#vent").click(function(){
+      $(".ventcont").toggle();
+    });
+    $("#desvent").click(function(){
+        $(".desventcont").toggle();
+      });
+  })
